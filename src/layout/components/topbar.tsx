add4ad405e2fb2681a3728a7917c7f50e3f46770
claralorusso <claralorusso@outlook.it>
2025-03-21 17:15:53 +0100

    feat: added home icon redirect

--- src/layout/components/topbar.tsx
+++ src/layout/components/topbar.tsx
@@ -1,6 +1,11 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
-
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import { useNavigate } from "react-router-dom";
 export const Topbar = () => {
+  const navigate = useNavigate();
+  const handleHomeClick = () => {
+    navigate("/");
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -8,6 +13,16 @@ export const Topbar = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Coldsharing - Help
           </Typography>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            onClick={handleHomeClick}
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <HomeIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
